6e9e1ec62875a0c6918daf69f4b468fd92e2447f
jaehle06 <jaehle06@gmail.com>
2025-05-20 13:50:22 +0900

    first table core_fct_events

---
+++ models/example/fact/core_fct_events.sql
@@ -0,0 +1,21 @@
+SELECT  
+  distinct
+  PARSE_DATE('%Y%m%d', event_date) AS date,
+  DATETIME(TIMESTAMP_MICROS(event_timestamp), 'Asia/Seoul') AS datetime,
+  COALESCE(user_pseudo_id, 'UNK') AS user_pseudo_id,
+  COALESCE((SELECT value.int_value FROM UNNEST (event_params) WHERE key = 'ga_session_id'), -1) AS ga_session_id,
+  COALESCE((SELECT value.int_value FROM UNNEST (event_params) WHERE key = 'ga_session_number'), -1) AS ga_session_number,
+  event_name,
+  COALESCE(device.web_info.hostname, 'UNK') AS hostname,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'page_location'), 'UNK') AS page_location_full,        
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'page_title'), 'UNK') AS page_title,
+  COALESCE(traffic_source.name, 'UNK') AS first_utm_campaign,
+  COALESCE(traffic_source.medium, 'UNK') AS first_utm_medium,
+  COALESCE(traffic_source.source, 'UNK') AS first_utm_source,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'campaign_id'), 'UNK') AS event_utm_campaign_id,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'campaign'), 'UNK') AS event_utm_campaign,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'medium'), 'UNK') AS event_utm_medium,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'source'), 'UNK') AS event_utm_source,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'term'), 'UNK') AS event_utm_term,
+  COALESCE((SELECT value.string_value FROM UNNEST (event_params) WHERE key = 'content'), 'UNK') AS event_utm_content,
+FROM `practice-215909.analytics_336023799.events_20250519`
